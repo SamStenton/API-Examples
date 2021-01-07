699f8c6f4348c58bdbf81f7d9d2ce1dc475eaaf8
Harry Fitzgerald <harry.f@elanco.com>
2021-01-07 14:08:25 +0000

    split out and godoc

--- go/main.go
+++ go/main.go
@@ -1,3 +1,4 @@
+// example HTTP GET and decode
 package main
 
 import (
@@ -8,28 +9,48 @@ import (
 	"encoding/json"
 )
 
-func main() {
-	fmt.Println("Hello, Padiwan")
-	resp, err := http.Get("https://engineering-task.elancoapps.com/api/applications")
+// Given an api url, make a GET and decode it.
+func fetchfromApi(apiUrl string) []byte {
+	resp, err := http.Get(apiUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// close the body after we're done with it
+	// close the body after we're done with it - post-return
 	defer resp.Body.Close()
 
 	raw, _ := ioutil.ReadAll(resp.Body)
+	return raw
 
-	body := string(raw)
+}
 
+// decode a string array in to a []string
+func decodeArray(body string) []string {
 	var arr []string
 
 	json.Unmarshal([]byte(body), &arr)
 
-	fmt.Printf("Found %d apps\n\n", len(arr))
+	return arr
+}
 
-	for _, app := range arr {
+// loop through apps and print them nicely
+func printApps(apps []string) {
+	fmt.Printf("Found %d apps\n\n", len(apps))
+	for _, app := range apps {
 		fmt.Printf(" > %s\n", app)
 	}
+}
+
+// main code init
+func main() {
+	apiUrl := "https://engineering-task.elancoapps.com/api/applications"
+
+	resp := fetchfromApi(apiUrl)
+
+	body := string(resp)
+
+	apps := decodeArray(body)
+
+	printApps(apps)
 }
 
